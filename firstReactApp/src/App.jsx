25f71decdd9b13bf10713e8b8145f18323cf9fdc
bdrozek <bdrozek@ttu.edu>
2024-09-16 17:07:00 -0500

    test

---
+++ firstReactApp/src/App.jsx
@@ -0,0 +1,14 @@
+import test from './Components/test'
+
+
+function App() {
+
+  return (
+    <>
+      <test/>
+        
+    </>
+  )
+}
+
+export default App;
---
+++ reactTest/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import BlobTest from "./Components/BlobTest";
+
+
+function App(){
+
+  return(
+      <>
+      
+      <BlobTest/>
+      </>
+    
+
+  );
+
+}
+
+export default App
